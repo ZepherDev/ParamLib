b4e9a624758a2c7401b1bbfc35d4d718fb5ad7d2
Ben <benaclejames@gmail.com>
2021-05-05 02:15:23 +0100

    Fix for latest vrchat update

--- ParamLib.cs
+++ ParamLib.cs
@@ -1,23 +1,23 @@
-﻿using VRC.SDK3.Avatars.ScriptableObjects;
+using VRC.SDK3.Avatars.ScriptableObjects;
+// ReSharper disable Unity.NoNullPropagation        Rider please, we both know this wasn't gonna be pretty...
 
 namespace ParamLib
 {
     public static class ParamLib
-    {
-        private static AvatarPlayableController GetLocalPlayableController() => VRCPlayer.field_Internal_Static_VRCPlayer_0
-            ?.field_Private_VRC_AnimationController_0
-            ?.field_Private_AvatarAnimParamController_0
+    {   
+        private static AvatarPlayableController LocalPlayableController => LocalAnimParamController
             ?.field_Private_AvatarPlayableController_0;
-        
-        private static AvatarAnimParamController GetLocalAnimParamController() => VRCPlayer.field_Internal_Static_VRCPlayer_0
-            ?.field_Private_VRC_AnimationController_0?.field_Private_AvatarAnimParamController_0;
+
+        private static AvatarAnimParamController LocalAnimParamController => VRCPlayer
+            .field_Internal_Static_VRCPlayer_0
+            ?.field_Private_AnimatorControllerManager_0?.field_Private_AvatarAnimParamController_0;
         
         public static void PrioritizeParameter(int paramIndex)
         {
-            var controller = GetLocalPlayableController();
+            var controller = LocalPlayableController;
             if (controller == null) return;
             
-            controller.Method_Public_Void_Int32_1(paramIndex);
+            controller.Method_Public_Void_Int32_0(paramIndex);
         }
         
         public static int? GetParamIndex(string paramName)
@@ -43,10 +43,10 @@ namespace ParamLib
         
         public static bool SetParameter(int paramIndex, float value)
         {
-            var controller = GetLocalAnimParamController();
+            var controller = LocalAnimParamController;
             if (controller?.field_Private_AvatarPlayableController_0 == null) return false;
 
-            controller.field_Private_AvatarPlayableController_0.Method_Public_Boolean_Int32_Single_0((int)paramIndex, value);
+            controller.field_Private_AvatarPlayableController_0.Method_Public_Boolean_Int32_Single_0(paramIndex, value);
             return true;
         }
     }
