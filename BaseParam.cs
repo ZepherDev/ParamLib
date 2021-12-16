6fcb36d087c66b6a1738b393c03f8053af2fb3be
Ben <benaclejames@gmail.com>
2021-12-16 22:59:25 +0000

    Removed XYParam in favor of people making their own x and y classes if and when needed. It's this or I make my own vector2 class and include it in ParamLib, which I feel like most people probably won't want.

--- BaseParam.cs
+++ BaseParam.cs
@@ -122,36 +122,4 @@ namespace ParamLib
             }
         }
     }
-
-    public class XYParam
-    {
-        public FloatBaseParam X, Y;
-
-        protected Vector2 ParamValue
-        {
-            set
-            {
-                X.ParamValue = value.x;
-                Y.ParamValue = value.y;
-            }
-        }
-
-        protected XYParam(FloatBaseParam x, FloatBaseParam y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        protected void ResetParams()
-        {
-            X.ResetParam();
-            Y.ResetParam();
-        }
-
-        protected void ZeroParams()
-        {
-            X.ZeroParam();
-            Y.ZeroParam();
-        }
-    }
 }
